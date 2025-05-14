006a0c3f2add1794f171a5936e1da3cef35a0d67
duttaturja <duttaturja@gmail.com>
2025-05-15 01:34:34 +0600

    utils added

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export function getNonce() {
+  let text = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 32; i++) {
+    text += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return text;
+}
